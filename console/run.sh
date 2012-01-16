406ca6c72323b4b1bfdb83060e4c9d22a57f3217
Cosmin Stejerean <cosmin@offbytwo.com>
2012-01-17 09:49:50 +1100

    added exec plugin so we can start repl with mvn exec:java

--- console/run.sh
+++ console/run.sh
@@ -1,2 +1,1 @@
-mvn compile
-java -cp ~/.m2/repository/net/sf/jline/jline/2.6-SNAPSHOT/jline-2.6-SNAPSHOT.jar:/Users/cosmin/.m2/repository/org/clojure/clojure/1.4.0-master-SNAPSHOT/clojure-1.4.0-master-SNAPSHOT.jar:target/classes Main
+mvn compile exec:java
